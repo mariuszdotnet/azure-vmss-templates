5bd30f6d78f546a6784b0a403d900224fd1707ff
mariuszdotnet <mariusz.kolodziej@outlook.com>
2016-10-24 00:19:05 -0400

    Updates to simple RHEL and VMSS RHEL

--- vm-simple-rhel/azuredeploy.ps1
+++ vm-simple-rhel/azuredeploy.ps1
@@ -1,13 +1,13 @@
 # Variables that need to be set
 
 # The subscriptionId
-$subscriptionId = '<SUBCRIPTIONID>'
+$subscriptionId = '<SUBSCRIPTIONID>'
 # Root path to script, template and parameters.  All have to be in the same folder.
-$rootPath = '<PATHTOSCRIPT>' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
+$rootPath = '<PATHTOSCRIPTS>' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
 # Name of the resource group
 $resourceGroupName = '<RGNAME>'
 # Resource Group Location 
-$resourceGroupLocation = '<RGLOCATION>' # Run <Get-AzureLocation> to find out azure locations; EXAMPLE: 'East US 2'
+$resourceGroupLocation = '<RGLOCATIONS>' # Run <Get-AzureLocation> to find out azure locations; EXAMPLE: 'East US 2'
 # Name of the deployment
 $deploymentName = '<DEPLOYMENTNAME>'
 
@@ -21,13 +21,10 @@ Select-AzureSubscription -SubscriptionId $subscriptionId
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
 
 # Run the below to test the ARM template
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json"
+# Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json"
 
 # Use parameter file
 # New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json" -TemplateParameterFile "$rootPath\azuredeploy.parameters.json"
 
 # Input parameters manually via CLI
 New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json"
-
-# Delete the deployment
-# Remove-AzureRmResourceGroup $resourceGroupName
