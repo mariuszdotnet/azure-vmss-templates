a4bd1355fd026ec7648312e431038a27e6c5604f
mariuszdotnet <mariusz.kolodziej@outlook.com>
2016-10-30 02:37:03 -0400

    updated to use existing vnet/subnet

--- vmss-redhat-ilb-jumpbox/deploy.ps1
+++ vmss-redhat-ilb-jumpbox/deploy.ps1
@@ -1,15 +1,15 @@
 # Variables that need to be set
 
 # The subscriptionId
-$subscriptionId = '<SUBCRIPTIONID>'
+$subscriptionId = '5aec60e9-f535-4bd7-a951-2833f043e918'
 # Root path to script, template and parameters.  All have to be in the same folder.
-$rootPath = '<PATHTOSCRIPT>' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
+$rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vmss-redhat-ilb-jumpbox' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
 # Name of the resource group
-$resourceGroupName = '<RGNAME>'
+$resourceGroupName = 'vmss-rg'
 # Resource Group Location 
-$resourceGroupLocation = '<RGLOCATION>' # Run <Get-AzureLocation> to find out azure locations; EXAMPLE: 'East US 2'
+$resourceGroupLocation = 'East US 2' # Run <Get-AzureLocation> to find out azure locations; EXAMPLE: 'East US 2'
 # Name of the deployment
-$deploymentName = '<DEPLOYMENTNAME>'
+$deploymentName = 'applybite'
 
 Login-AzureRmAccount
 
@@ -21,7 +21,7 @@ Select-AzureSubscription -SubscriptionId $subscriptionId
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
 
 # Run the below to test the ARM template
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json"
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json" -TemplateParameterFile "$rootPath\azuredeploy.parameters.json"
 
 # Use parameter file
 # New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json" -TemplateParameterFile "$rootPath\azuredeploy.parameters.json"
